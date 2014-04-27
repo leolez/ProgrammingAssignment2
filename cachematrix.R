4d708b1fd6910bbfce7a62940f0ac3a2b9476547
leolez <lezcano.leo@gmail.com>
2014-04-27 18:16:10 -0400

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## The following to functions improve the performance of the 'solve()' when it is used
+## for the purpose of calculating the inverse of a matrix, by means of encapsulation and 
+## catching
+##
 
-## Write a short comment describing this function
+## The makeCache Matrix function returns a 'list' including the functions 
+## 'set', 'get', 'setInvMat' and 'getInvMat' that encapsulate the interaction
+## with the matrix received as parameter and make the caching process as transparent
+## as possible for the user, following OOP patterns.
 
 makeCacheMatrix <- function(x = matrix()) {  
   invmat <- NULL
@@ -18,7 +24,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve()' function updates the inverse of matrix only when there is 
+## no previous calculation of the inverse for that same matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
